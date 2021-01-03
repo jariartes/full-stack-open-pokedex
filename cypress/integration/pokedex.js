ad2690bf541481cfadb5f6a7773a954b844a9ce5
Jari Artes <jari.artes@gmail.com>
2021-01-03 18:04:03 +0200

    ex11.9.4

--- cypress/integration/pokedex.js
+++ cypress/integration/pokedex.js
@@ -1,7 +1,17 @@
 describe('Pokedex', function () {
-  it('front page can be opened', function () {
+
+  beforeEach(function () {
     cy.visit('http://localhost:5000')
+  })
+
+  it('front page can be opened', function () {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+
+  it('ivysaur can be visited and it contains the correct stuff', function () {
+    cy.contains('ivysaur').click()
+    cy.contains('chlorophyll')
+  })
+
 })
